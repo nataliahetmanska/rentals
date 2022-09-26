31698999308cf15cdccdea62091c3ee36f20b04e
Ilia Gilmijarow <ilia.gilmijarow@capgemini.com>
2022-09-26 17:26:18 +0200

    cosmetic changes

--- test_funkcja.py
+++ test_funkcja.py
@@ -2,6 +2,7 @@ from funkcja import generate_rentals
 import unittest
 import random as rnd
 
+
 class TestRentalGenerator(unittest.TestCase):
 
     def test_generate_rentals_happy_path (self):
@@ -14,7 +15,6 @@ class TestRentalGenerator(unittest.TestCase):
 
         self.assertIsInstance(result, list)
 
-
     def test_generate_rentals_when_nothing_is_rented(self):
         inv = set(range(30))
         rented = set(rnd.sample(range(30), k=20))
@@ -26,9 +26,7 @@ class TestRentalGenerator(unittest.TestCase):
         self.assertEqual(result, [list() for _ in range(30)])
 
 
-
 if __name__ == '__main__':
-
     unittest.main()
 
 
