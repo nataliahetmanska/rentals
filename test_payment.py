f85bd917d677762868d41412fd8150245e6d38c0
anastasiakruk78 <113979949+anastasiakruk78@users.noreply.github.com>
2022-10-17 12:26:21 +0200

    Update test_payment.py

--- test_payment.py
+++ test_payment.py
@@ -50,13 +50,19 @@ class TesPaymentGenerator(unittest.TestCase):
     def test_get_rentals(self):
         payment.connection = fake_connection_get_rentals
         latest_date_from_db = datetime.date(2017, 3, 4)
-        rental_id, rental_rate, customer_id, rental_date, return_date, payment_deadline = payment.get_rentals(latest_date_from_db)
+        rental_id, rental_rate, customer_id, rental_date, return_date, payment_deadline, amount = payment.get_rentals(latest_date_from_db)
         self.assertEqual(rental_id, [1300001, 1300007])
         self.assertEqual(rental_rate, [Decimal('149.00'), Decimal('149.00')])
         self.assertEqual(customer_id, [94714, 89117])
         self.assertEqual(rental_date, [datetime.date(2017, 2, 25), datetime.date(2016, 2, 13)])
         self.assertEqual(return_date, [datetime.date(2017, 3, 6), datetime.date(2016, 2, 17)])
         self.assertEqual(payment_deadline, [datetime.date(2017, 3, 4), datetime.date(2016, 2, 20)])
+        
+    def test_max_payment_id(self):
+        payment.connection = fake_connection_max_payment_id
+        result = payment.max_payment_id()
+        expected_result = 4
+        self.assertEqual(result, expected_result)
         
 
 def random_randint(x, y):
@@ -75,6 +81,17 @@ class FakeCursorGetRentals():
 
 def fake_connection_get_rentals():
     fake_cursor = FakeCursorGetRentals()
+    return None, fake_cursor
+
+class FakeCursorMaxPaymentId():
+    def execute(self, _):
+        pass
+
+    def fetchall(self):
+        return [(1,), (2,), (3,), (4,)]
+
+def fake_connection_max_payment_id():
+    fake_cursor = FakeCursorMaxPaymentId()
     return None, fake_cursor
 
 if __name__ == '__main__':
