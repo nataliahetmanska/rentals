ba097509b83e1f69b79708ceb2630685405a6928
Sławomir Wojciechowski <80217159+swojciechowski7@users.noreply.github.com>
2022-10-25 16:14:14 +0200

    cosmetic changes

--- test_service.py
+++ test_service.py
@@ -1,56 +1,54 @@
-import service
+import services
 import unittest
 import datetime
 from freezegun import freeze_time
 
+
 class TestServicesGenerator(unittest.TestCase):
 
     def test_get_cars_in_stock(self):
-        service.interaction.connection = fake_connection_get_last_service_id
+        services.interaction.connection = fake_connection_get_last_service_id
         service_date1 = datetime.datetime(2015, 2, 1, 0, 0)
         service_date2 = datetime.datetime(2017, 2, 1, 0, 0)
         service_date3 = datetime.datetime(2020, 2, 1, 0, 0)
-        result1 = service.get_cars_in_stock(service_date1)
-        result2 = service.get_cars_in_stock(service_date2)
-        result3 = service.get_cars_in_stock(service_date3)
+        result1 = services.get_cars_in_stock(service_date1)
+        result2 = services.get_cars_in_stock(service_date2)
+        result3 = services.get_cars_in_stock(service_date3)
         self.assertEqual(result1, [])
         self.assertEqual(result2, [1, 2])
         self.assertEqual(result3, [3])
-    
+
     def test_get_rented_cars(self):
-        service.interaction.connection = fake_connection_get_rented_cars
+        services.interaction.connection = fake_connection_get_rented_cars
         service_date1 = datetime.datetime(2017, 2, 14, 0, 0)
-        result = service.get_rented_cars(service_date1)
-        expected_result = [{1: datetime.date(2017, 2, 16)}, {5: datetime.date(2017, 2, 22)}, {6: datetime.date(2017, 2, 15)}]
+        result = services.get_rented_cars(service_date1)
+        expected_result = [{1: datetime.date(2017, 2, 16)}, {5: datetime.date(2017, 2, 22)},
+                           {6: datetime.date(2017, 2, 15)}]
         self.assertEqual(result, expected_result)
-        
+
     def test_check_if_car_is_rented(self):
         inv_id = 5
         service_date = datetime.datetime(2017, 1, 10, 0, 0)
-        result = service.check_if_car_is_rented(inv_id, service_date, get_rented_cars_mp)
+        result = services.check_if_car_is_rented(inv_id, service_date)
         expected_result = 5, datetime.date(2017, 1, 11)
-        self.assertEqual(result, expected_result)    
-    
+        self.assertEqual(result, expected_result)
 
     @freeze_time("Aug 29th, 2018")
     def test_create_service_dates(self):
         expected_result = [datetime.datetime(2016, 7, 1, 0, 0), datetime.datetime(2017, 1, 3, 0, 0),
                            datetime.datetime(2017, 7, 8, 0, 0), datetime.datetime(2018, 1, 10, 0, 0),
                            datetime.datetime(2018, 7, 15, 0, 0)]
-        result = service.create_service_dates()
+        result = services.create_service_dates()
         self.assertEqual(result, expected_result)
-        
+
     @freeze_time("Aug 29th, 2018")
     def test_create_tire_change_dates(self):
         expected_result = [datetime.datetime(2016, 3, 15, 0, 0), datetime.datetime(2016, 11, 15, 0, 0),
                            datetime.datetime(2017, 3, 15, 0, 0), datetime.datetime(2017, 11, 15, 0, 0),
                            datetime.datetime(2018, 3, 15, 0, 0)]
-        result = service.create_tire_change_dates()
+        result = services.create_tire_change_dates()
         self.assertEqual(result, expected_result)
-        
-    
-        
-        
+
 
 class FakeCursorLastServiceId():
     def execute(self, _):
@@ -82,8 +80,10 @@ def fake_connection_get_rented_cars():
     fake_cursor = FakeCursorRentedCars()
     return None, fake_cursor
 
+
 def test_datetime():
-    assert date.today() == date(2018, 8, 29)
-    
+    assert datetime.date.today() == datetime.date(2018, 8, 29)
+
+
 def get_rented_cars_mp(service_date):
     return [{1: datetime.date(2017, 1, 16)}, {5: datetime.date(2017, 1, 11)}, {6: datetime.date(2017, 1, 15)}]
